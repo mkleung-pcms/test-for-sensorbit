0599319cb5f2fc4a8796c76430e959d3e7039994
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-11-30 22:53:56 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     status = 1 - status
 })
+let level = 0
 let status = 0
 status = 1
 basic.forever(function () {
+	
+})
+loops.everyInterval(200, function () {
+    level = sensors.Ultrasonic(DigitalPin.P15)
     if (status) {
+        TM1650.off()
         led.plotBarGraph(
-        sensors.slideRheostat(AnalogPin.P0),
-        1023
+        level,
+        255
         )
     } else {
-        basic.showNumber(sensors.slideRheostat(AnalogPin.P0))
+        basic.clearScreen()
+        TM1650.on()
+        TM1650.showNumber(level)
     }
-    if (sensors.slideRheostat(AnalogPin.P0) > 200) {
-        pins.analogWritePin(AnalogPin.P1, sensors.slideRheostat(AnalogPin.P0))
+    if (level < 50) {
+        sensors.sensorbit_rus04(DigitalPin.P16, RgbUltrasonics.All, RgbColors.Orange, ColorEffect.Breathing)
+        music.setVolume(255)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        pins.analogWritePin(AnalogPin.P1, 511)
     } else {
+        music.stopAllSounds()
         pins.analogWritePin(AnalogPin.P1, 0)
     }
 })
